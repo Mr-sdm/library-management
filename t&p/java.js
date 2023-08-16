e0d4eb59ebf9c3943286d666e714fec4bf28553e
Mr-sdm <142393824+Mr-sdm@users.noreply.github.com>
2023-08-16 18:36:24 +0530

    Add files via upload

---
+++ t&p/java.js
@@ -0,0 +1,20 @@
+function dodo()
+{
+    const a=document.getElementById('login');
+    a.style.clipPath="circle(300% at 0 0)";
+}
+function gogo() 
+{
+    const a=document.getElementById('login');
+    a.style.clipPath="circle(0% at 0 0)";
+}
+function show() 
+{
+    const b=document.getElementById("signup");
+    b.style.clipPath="circle(300% at 100% 100%)";
+}
+function momo() 
+{
+    const b=document.getElementById("signup");
+    b.style.clipPath="circle(0% at 100% 100%)";
+}
